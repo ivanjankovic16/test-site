7c5c9f875cd2c5d731d6a614cc45a2d6831b8101
ivanjankovic16 <64977238+ivanjankovic16@users.noreply.github.com>
2020-07-14 16:28:36 +0200

    Add files via upload
    Repository za testiranje web stranica

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/hvar.jpg') {
+      myImage.setAttribute ('src','images/hvar_plaza.jpg');
+    } else {
+      myImage.setAttribute ('src','images/hvar.jpg');
+    }
+}
+
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+
+function setUserName() {
+	let myName = prompt('Please enter your name.');
+	if (!myName) {
+		setUserName();
+	}	else {
+	localStorage.setItem('name', myName);
+	myHeading.innerHTML = 'Hvar is cool, ' + myName;
+	}
+}
+
+
+if(!localStorage.getItem('name')) {
+	setUserName();
+}	else {
+	let storedName = localStorage.getItem('name');
+	myHeading.textContent = 'Hvar is cool, ' + storedName;
+}
+
+
+myButton.onclick = function() {
+	setUserName();
+}
